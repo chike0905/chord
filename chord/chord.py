86773f198e79a5af8bba7b9e95010914130a38cc
Ryosuke Abe <chike@sfc.wide.ad.jp>
2021-05-12 22:07:47 +0900

    tmp commit

---
+++ chord/chord.py
@@ -0,0 +1,140 @@
+import hashlib
+import ipaddress
+
+import grpc
+from chord.grpcprotos import peer_pb2
+from chord.grpcprotos import peer_pb2_grpc  
+
+from typing import List, Union, Optional
+
+
+KEYLENGTH = 256  # Note: we use sha256 hash as a key
+
+# Calc tools
+class Key():
+    value: str
+
+    def __init__(self, value: str) -> None:
+        if len(value) != 64:
+            raise Exception
+        self.value = value
+
+def addKey(key1: Key, key2: Key) -> Key: 
+    newvalue = int(key1.value, 16) + int(key2.value, 16)
+    if len(hex(newvalue)[2:]) > 64:
+        newvalue = newvalue - int("".ljust(64, "f"), 16) - 1
+    return Key(hex(newvalue)[2:].zfill(64))
+
+def isBetween(start: Key, end: Key, target: Key) -> bool:
+    if start.value < end.value:
+        return start.value < target.value <= end.value
+    else: # start.value >= end.value
+        return start.value < target.value or target.value <= end.value
+
+class Peer():
+    ip: ipaddress.IPv4Address
+    port: int
+    id: Key
+
+    def __init__(self, ip: ipaddress.IPv4Address, port: int, id: Key) -> None:
+        self.ip = ip
+        self.port = port
+        self.id = id
+        
+
+class Finger():
+    start: Key
+    interval: Optional[bytes]
+    node: Optional[Peer]
+    
+    def __init__(self, start: Key, interval: bytes = None, node: Peer = None) -> None:
+        self.start = start
+        self.interval = interval
+        self.node = node
+
+
+class FingerTable():
+    successor: Peer
+    predecessor: Peer
+    fingers: List[Finger]
+    
+    def __init__(self, node_id: Key) -> None:
+        self._initFingers(node_id)
+
+    def _initFingers(self, node_id: Key) -> None:
+        self.fingers = [self._initFinger(i, node_id) for i in range(0, KEYLENGTH)]
+    
+    def _initFinger(self, index: int, node_id: Key) -> Finger:
+        start_key = addKey(node_id, Key(hex(2**index)[2:].zfill(64)))
+        return Finger(start_key)
+
+class Node():
+    ip: ipaddress.IPv4Address
+    port: int
+    id: Key 
+    table: FingerTable
+    
+    def __init__(self, ip: ipaddress.IPv4Address, port: int, initialpeer: Optional[Peer] = None) -> None:
+        self.ip = ip
+        self.port = port
+        self.id = self._generateNodeId()
+        self.table = FingerTable(self.id)
+
+        self._initFingerTable(initialpeer)
+    
+    def _initFingerTable(self, initialpeer: Optional[Peer]) -> None:
+        if initialpeer:
+            print("Note: Not implemented")
+        else:
+            for i in range(KEYLENGTH):
+                self.table.fingers[i].node = Peer(self.ip, self.port, self.id)
+            self.table.successor = self.table.fingers[0].node
+            self.table.predecessor = Peer(self.ip, self.port, self.id)
+
+    def _generateNodeId(self) -> Key:
+        return Key(hashlib.sha256(self.ip.packed).hexdigest())
+
+    def findSuccessor(self, key: Key) -> Key:
+        predecessor: Peer = self.findPredecessor(key)
+        return predecessor.successor()
+
+    def findPredecessor(self, key: Key) -> Peer:
+        node = Peer(self.ip, self.port, self.id) 
+        suc = self.table.successor.id
+        while not isBetween(node.id, suc, key):
+            print("not implemented")
+        return node
+
+class NodeServicer(peer_pb2_grpc.PeerServicer):
+    def __init__(self, ip: ipaddress.IPv4Address, port: int) -> None:
+        super().__init__()
+        self.node = Node(ip, port)
+
+    def getSuccessor(self, request, context): #type: ignore
+        return peer_pb2.Successor(suc_id="hoge", suc_ip=b"aaa", suc_port=114514)
+
+from concurrent import futures
+import time
+
+_ALIVE_CHECK_INTERVAL = 1
+class NodeServer():
+    def __init__(self, ip: ipaddress.IPv4Address, port: int) -> None:
+        self.servicer = NodeServicer(ip, port)
+        self.alive = True
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        peer_pb2_grpc.add_PeerServicer_to_server(self.servicer, self.server) #type: ignore
+        self.server.add_insecure_port("[::]:"+str(port))
+        
+    def serve(self) -> None:
+        self.server.start()
+        try:
+            while True:
+                time.sleep(_ALIVE_CHECK_INTERVAL)
+                if not self.alive:
+                    break
+        except KeyboardInterrupt:
+            self.stop()        
+
+    def stop(self) -> None:
+        self.server.stop(0)
+        self.alive = False
---
+++ tests/test_node.py
@@ -0,0 +1,106 @@
+from chord.chord import *
+
+import pytest
+import ipaddress
+import hashlib
+
+# Node A
+# id: 5b9b2b3122bdac7e303b15bfc5895c10204de107f16bd16104f24184583c13ed
+IP = ipaddress.IPv4Address("192.168.56.1")
+PORT = 3343
+
+# Node B
+# id: 1158ec9de62eb3c22228738cdfc05e90a98a9018de91b229f9ab5cb9e690af51
+B_IP = ipaddress.IPv4Address("192.168.56.2")
+B_PORT = 3343
+
+
+
+def test_key() -> None:
+    key1 = Key("5b9b2b3122bdac7e303b15bfc5895c10204de107f16bd16104f24184583c13ed")
+    with pytest.raises(Exception):
+        badkey = Key("d")
+
+def test_add_key() -> None:
+    key1 = Key("1".zfill(64))
+    key2 = Key("f".zfill(64))
+    res = Key("10".zfill(64))
+
+    added_key = addKey(key1, key2)
+    assert added_key.value == res.value
+    
+    # Overflow
+    key1 = Key("1".ljust(64, "0"))
+    key2 = Key("f".ljust(64, "0"))
+    res = Key("0".zfill(64))
+    
+    added_key = addKey(key1, key2)
+    assert added_key.value == res.value
+    
+    key1 = Key("1".zfill(64))
+    key2 = Key("".ljust(64, "f"))
+    res = Key("0".zfill(64))
+    
+    added_key = addKey(key1, key2)
+    assert added_key.value == res.value
+
+def test_isBetween() -> None:
+    key1 = Key("1".ljust(64, "0"))  # 1000...0000
+    key2 = Key("f".ljust(64, "0"))  # f000...0000
+    
+    key = Key("2".ljust(64, "0"))   # 2000...0000
+    assert isBetween(key1, key2, key)
+    assert not isBetween(key2, key1, key)
+    
+    key = Key("1".zfill(64))  # 0000...0001
+    assert not isBetween(key1, key2, key)
+    assert isBetween(key2, key1, key)
+
+    
+
+def test_init_node() -> None:
+    node = Node(IP, PORT)
+    assert node.ip == IP
+    assert node.port == PORT
+
+    nodeid = hashlib.sha256(IP.packed).hexdigest()
+    assert nodeid == node.id.value
+
+    assert nodeid == node.table.successor.id.value
+
+    for finger in node.table.fingers:
+        assert nodeid == finger.node.id.value
+
+def test_findSuccessor() -> None:
+    node = Node(IP, PORT)# id: 5b9b2b3122bdac7e303b15bfc5895c10204de107f16bd16104f24184583c13ed
+    key = Key("1".zfill(64))
+    #suc = node.findSuccessor(key)
+    #assert suc.id.value == node.id.value
+
+def test_NodeServicer() -> None:
+    NodeServicer(IP, PORT)
+
+import threading
+@pytest.fixture(scope="function")
+def node():
+    server = NodeServer(IP, PORT)
+    thread = threading.Thread(None, server.serve)
+    thread.start()
+
+    yield
+
+    server.stop()
+    thread.join()
+    
+from chord.grpcprotos import peer_pb2
+from chord.grpcprotos import peer_pb2_grpc  
+def test_ServeGetSuccessor(node) -> None:
+    channel = grpc.insecure_channel("[::]:" + str(PORT))
+    stub = peer_pb2_grpc.PeerStub(channel)
+
+    node_id = "hoge"
+
+    response = stub.getSuccessor(peer_pb2.GetSuccessor(id=node_id))
+    print(f"Successor ID: {response.suc_id}")
+    print(f"Successor IP: {response.suc_ip}")
+    print(f"Successor Port: {response.suc_port}")
